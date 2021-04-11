3bdf9091fea6f7747195af3a8569d4645f755286
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-11 21:50:31 +0530

    Intellij setup for selenium

---
+++ Simple/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        // chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "D:\\ProgramData\\projects\\java\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://login.salesforce.com/");
+        driver.findElement(By.id("username")).sendKeys("hello");
+        driver.findElement(By.name("pw")).sendKeys("hello");
+        // driver.findElement(By.className("button r4 wide primary")).click();
+        driver.findElement(By.xpath("//*[@id=\'Login\']")).click();
+        System.out.println(driver.findElement(By.cssSelector("div#error.loginError")).getText());
+    }
+}
