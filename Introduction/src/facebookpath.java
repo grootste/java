5151f6bddccd3e945cdf87b4d2a72a3e2765d257
Baibhav Singh <singh.baibhav321@gmail.com>
2023-04-01 13:55:27 +0545

    Checking synchronization

--- Introduction/src/facebookpath.java
+++ Introduction/src/facebookpath.java
@@ -1,8 +1,8 @@
 import java.util.List;
 
-import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 public class facebookpath {
 
 	public static void main(String[] args) {
@@ -16,7 +16,7 @@ public class facebookpath {
 		driver.findElement(By.xpath("//*[@type='text']")).sendKeys("customized xpath");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("customized pass Xpath");
 		driver.findElement(By.xpath("//button[@name='login']")).click();
-		System.out.println(driver.findElement(By.xpath("//a[@href='/login/identify/']")).getText());
+		//System.out.println(driver.findElement(By.xpath("//a[@href='/login/identify/']")).getText());
 		
 		
 		
